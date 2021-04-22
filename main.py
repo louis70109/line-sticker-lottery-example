753234fd36e8ea09eaa358d07af0fa806c8e12c4
louis70109 <louis70109@gmail.com>
2021-04-22 15:57:31 +0800

    🚑️hange port

--- main.py
+++ main.py
@@ -31,4 +31,6 @@ async def read_item(request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0"
+                # , port=5000, reload=True
+                )
